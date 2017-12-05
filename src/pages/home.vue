37fc55761106be5c2cc748b2958f4a966574bf9e
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-12-05 10:36:47 +0800

    updated landing page

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,10 @@
 <template lang="html">
   <main class="article home">
     <div class="wrapper">
-      <h1 v-for="item in phrases"> {{ item }} </h1>
+      <h1>
+          <span id="output"> </span><span class="blink">|</span>
+       </h1>
+
     </div>
   </main>
 </template>
@@ -11,14 +14,45 @@ export default {
   data(){
     return {
       phrases : [
-        'Hello World',
+        'Hello World! ',
         'I\'m Jean Andrew Fuentes ',
-        'Web Developer'
+        'Web Developer  '
       ]
     }
   },
   mounted(){
+    let textHolder = []
 
+    const output = document.querySelector('#output')
+    let i = 0, counter = 0
+    let value = this.phrases[counter].split('')
+    const typeEffect = () => {
+      const typeWriterInterval = setInterval(() => {
+      textHolder.push(value[i])
+      output.innerHTML = textHolder.join('')
+      i++
+      if(i >= value.length) {
+        clearInterval(typeWriterInterval)
+        const erase = setInterval(() => {
+              textHolder.pop()
+              output.innerHTML = textHolder.join('')
+
+              if (textHolder.length == 0){
+                clearInterval(erase)
+                i = 0
+                if ( (counter + 1) >= this.phrases.length ){
+                  counter = 0
+                } else {
+                  counter++
+                }
+                value = this.phrases[counter].split('')
+                setTimeout(typeEffect, 700)
+                }
+          }, 120)
+        }
+      }, 40)
+    }
+    setTimeout(typeEffect, 1000 )
   }
 
 }
