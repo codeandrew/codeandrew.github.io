a92442ff4711e4842a0363521c63480407ca1212
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-12-05 16:37:10 +0800

    updated social media structure and animations

--- src/components/social-media-list.vue
+++ src/components/social-media-list.vue
@@ -1,10 +1,13 @@
 <template lang="html">
   <div class="social-media">
     <ul>
-      <li v-for="item in list" >
-        <a :href="item.to" class="social-button" >
+      <li v-for="item in list">
+        <a :href="item.to" class="social-button">
           <i :class="item.class" class="spin circle" aria-hidden="true"></i>
         </a>
+        <div class="info">
+          <p> {{ item.name }}</p>          
+        </div>
       </li>
     </ul>
   </div>
@@ -17,15 +20,18 @@ export default {
       list : [
         {
           to: 'https://codepen.io/andrewfuentes/' ,
-          class : 'fa fa-codepen fa-3x'
+          class : 'fa fa-codepen fa-3x',
+          name: 'codepen'
         },
         {
           to: 'https://twitter.com/code_drew/' ,
-          class : 'fa fa-twitter fa-3x'
+          class : 'fa fa-twitter fa-3x',
+          name : 'twitter'
         },
         {
           to: 'https://www.linkedin.com/in/jeanandrew/' ,
-          class : 'fa fa-linkedin fa-3x'
+          class : 'fa fa-linkedin fa-3x',
+          name : 'linkedin'
         },
       ]
     }
