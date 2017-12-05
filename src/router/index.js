1fa94b07775da580e985372cf2c3dbab6f52b0e0
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-12-05 10:33:14 +0800

    deleted unnecessary page

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Home from '../pages/home'
 import About from '../pages/about'
-import Example from '../pages/example'
 
 Vue.use(Router)
 
@@ -19,11 +17,6 @@ export default new Router({
       path: '/about',
       name : 'about',
       component : About
-    },
-    {
-      path: '/example',
-      name : 'example',
-      component : Example
     }
   ]
 })
