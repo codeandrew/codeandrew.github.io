15013f8b835ae57f79ba86314e521ed6dd2597b2
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-12-05 17:27:15 +0800

    updated about page layout and style

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,13 +1,26 @@
 <template lang="html">
   <div class="about article">
     <b-container>
-    <h1>About Page</h1>
-    <br />
-      <p> This is a boiler Template of VueJs to reduce time in setting up and more on actual coding</p>
-      <p> together with: </p>
-      <ul class='about-list'>
-        <li v-for="item in dep"> {{ item }}</li>
-      </ul>
+    <div class="portrait">
+      <div class="img-holder">
+        <p class="text-clip">
+          {{ text_clip }}
+        </p>
+      </div>
+
+    </div>
+    <div class="about-me">
+      <h2> Jean Andrew Fuentes</h2>
+      <h3> Web Developer</h3>
+      <br />
+
+      <h4>About me</h4>
+      <p>{{ par }}</p>
+      <br />
+      <h4>About this Website</h4>
+      <p>{{ par }}</p>
+    </div>
+
     </b-container>
   </div>
 </template>
@@ -16,20 +29,13 @@
 export default {
   data() {
     return {
-      dep : [
-        'BootstrapVue',        
-        'VueRouter',
-        'Vuex',
-        'Sass/Scss'
-      ]
+      par : `Por pura amplitud de extrañas características anatómicas, el camaleón tiene pocos rivales. Una lengua mucho más larga que su cuerpo, disparando para atrapar insectos en una fracción de segundo. Ojos de visión telescópica que giran independientemente en las torretas abovedadas. Pies con dedos de los pies fusionados en pinzas tipo mitones. Cuernos que brotan de la frente y el hocico. Adornos nasales nudosos. Una aleta de piel que rodea el cuello como una puntilla de encaje sobre un noble isabelino.`,
+      text_clip : ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cumque consequuntur libero, neque optio voluptatem debitis molestiae nisi voluptas doloremque blanditiis quo modi tempora officiis temporibus itaque in saepe laboriosam nesciunt eos voluptatum molestias. Velit illo dolorum laboriosam ad necessitatibus possimus illum nisi nesciunt. Perspiciatis debitis totam natus sunt quasi accusantium et quae. Nisi ea quam minima nobis numquam magnam, ducimus dolore porro pariatur excepturi dolorem rerum laudantium. Soluta maxime quis vero nesciunt molestiae neque, ducimus, quia tio voluptatem debitis molestiae nisi voluptas doloremque blanditiis quo modi tempora officiis temporibus itaque in saepe laboriosam nesciunt eos voluptatum molestias. Velit illo dolorum laboriosam ad necessitatibus possimus illum nisi nesciunt. Perspiciatis debitis totam natus sunt quasi accusantium et quae. Nisi ea quam minima nobis numquam magnam, ducimus dolore porro pariatur excepturi dolorem rerum laudantium. Soluta maxime quis vero nesciunt molestiae neque, ducimus, quia tio voluptatem debitis molestiae nisi voluptas doloremque blanditiis quo modi tempora officiis temporibus itaque in saepe laboriosam nesciunt eos voluptatum molestias. Velit illo dolorum laboriosam ad necessitatibus possimus illum nisi nesciunt. Perspiciatis debitis totam natus sunt quasi accusantium et quae. Nisi ea quam minima nobis numquam magnam, ducimus dolore porro pariatur excepturi dolorem rerum laudantium. Soluta maxime quis vero nesciunt molestiae neque, ducimus, quia tio voluptatem debits doloremque blanditiis quo modi tempora officiis temporibus itaque in saepe laboriosam nesciunt eos voluptatum molestias. Velit illo dolorum laboriosam ad necessitatibus possimus illum nisi nesciunt. Perspiciatis debitis totam natus sunt quasi accusantium et quae. Nisi ea quam minima nobis numquam magnam, ducimus dolore porro pariatur excepturi dolorem rerum laudantium.  itaque veniam perspiciatis impedit iste ullam harum dignissimos nobis quidem recusandae. Quod ullam, repellendus rem qui ipsum similique suscipit atque laudantium, facere totam nostrum commodi optio eligendi ipsam deserunt aliquid, quae hic alias. Suscipit odit corrupti ex. Animi dicta delectus odit?  perspiciatis impedit iste ullam harum dignissimos nobis quidem recusandae. Quod ullam, repellendus rem qui ipsum similique suscipit atque laudantium, facere totam nostrum commodi optio eligendi ipsam deserunt aliquid, quae hic alias. Suscipit odit corrupti ex. Animi dicta delectus odit?  eveniet aliquid enim laborum esse itaque veniam perspiciatis impedit iste ullam harum dignissimos nobis quidtaque veniam perspiciatis impedit iste ullam harum dignissimos nobis quidit?  eveniet aliquid enim laborum esse itaque veniam perspiciatis impedit iste ullam harum dignissimos nobis qu`
     }
   }
 }
 </script>
 
 <style lang="css">
-.about-list {
-  width : 200px;
-  margin: 0 auto;
-}
+
 </style>
