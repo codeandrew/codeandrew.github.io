c988624fd43f951355cf94155a482103d4d3646b
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-12-07 11:11:38 +0800

    added skill page feature

---
+++ src/components/skill-list.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="skill-list">
+    <h4> {{ title }}</h4>
+    <ul>
+      <li v-for=" item in skill"> {{ item }} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props : ['skill', 'title']
+
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/skill-manager.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div class="skill-manager">
+    <h2>Skill Manager</h2>
+
+    <skill-list
+    title="Programming Languages"
+    :skill="programming_language"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      programming_language : ['JavaScript', 'PHP']
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
