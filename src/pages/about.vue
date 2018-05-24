c6f3215f32b1be4bc9df92157ad0d0f9a194957e
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-05-24 21:12:09 +0800

    Update about me paragraphs

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -15,10 +15,10 @@
       <br />
 
       <h4>About me</h4>
-      <p>{{ par }}</p>
+      <p>{{ about_me }}</p>
       <br />
       <h4>About this Website</h4>
-      <p>{{ par }}</p>
+      <p>{{ about_the_web }}</p>
     </div>
 
     </b-container>
@@ -29,8 +29,24 @@
 export default {
   data() {
     return {
+
       par : `Por pura amplitud de extrañas características anatómicas, el camaleón tiene pocos rivales. Una lengua mucho más larga que su cuerpo, disparando para atrapar insectos en una fracción de segundo. Ojos de visión telescópica que giran independientemente en las torretas abovedadas. Pies con dedos de los pies fusionados en pinzas tipo mitones. Cuernos que brotan de la frente y el hocico. Adornos nasales nudosos. Una aleta de piel que rodea el cuello como una puntilla de encaje sobre un noble isabelino.`,
-      text_clip : ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cumque consequuntur libero, neque optio voluptatem debitis molestiae nisi voluptas doloremque blanditiis quo modi tempora officiis temporibus itaque in saepe laboriosam nesciunt eos voluptatum molestias. Velit illo dolorum laboriosam ad necessitatibus possimus illum nisi nesciunt. Perspiciatis debitis totam natus sunt quasi accusantium et quae. Nisi ea quam minima nobis numquam magnam, ducimus dolore porro pariatur excepturi dolorem rerum laudantium. Soluta maxime quis vero nesciunt molestiae neque, ducimus, quia tio voluptatem debitis molestiae nisi voluptas doloremque blanditiis quo modi tempora officiis temporibus itaque in saepe laboriosam nesciunt eos voluptatum molestias. Velit illo dolorum laboriosam ad necessitatibus possimus illum nisi nesciunt. Perspiciatis debitis totam natus sunt quasi accusantium et quae. Nisi ea quam minima nobis numquam magnam, ducimus dolore porro pariatur excepturi dolorem rerum laudantium. Soluta maxime quis vero nesciunt molestiae neque, ducimus, quia tio voluptatem debitis molestiae nisi voluptas doloremque blanditiis quo modi tempora officiis temporibus itaque in saepe laboriosam nesciunt eos voluptatum molestias. Velit illo dolorum laboriosam ad necessitatibus possimus illum nisi nesciunt. Perspiciatis debitis totam natus sunt quasi accusantium et quae. Nisi ea quam minima nobis numquam magnam, ducimus dolore porro pariatur excepturi dolorem rerum laudantium. Soluta maxime quis vero nesciunt molestiae neque, ducimus, quia tio voluptatem debits doloremque blanditiis quo modi tempora officiis temporibus itaque in saepe laboriosam nesciunt eos voluptatum molestias. Velit illo dolorum laboriosam ad necessitatibus possimus illum nisi nesciunt. Perspiciatis debitis totam natus sunt quasi accusantium et quae. Nisi ea quam minima nobis numquam magnam, ducimus dolore porro pariatur excepturi dolorem rerum laudantium.  itaque veniam perspiciatis impedit iste ullam harum dignissimos nobis quidem recusandae. Quod ullam, repellendus rem qui ipsum similique suscipit atque laudantium, facere totam nostrum commodi optio eligendi ipsam deserunt aliquid, quae hic alias. Suscipit odit corrupti ex. Animi dicta delectus odit?  perspiciatis impedit iste ullam harum dignissimos nobis quidem recusandae. Quod ullam, repellendus rem qui ipsum similique suscipit atque laudantium, facere totam nostrum commodi optio eligendi ipsam deserunt aliquid, quae hic alias. Suscipit odit corrupti ex. Animi dicta delectus odit?  eveniet aliquid enim laborum esse itaque veniam perspiciatis impedit iste ullam harum dignissimos nobis quidtaque veniam perspiciatis impedit iste ullam harum dignissimos nobis quidit?  eveniet aliquid enim laborum esse itaque veniam perspiciatis impedit iste ullam harum dignissimos nobis qu`
+
+      about_me : `
+      Hi! I'm a Full Stack Web Developer. I specialize in JavaScript in particular.
+      I wasn't always been a programmer but ever since I was kid I love doing things
+      on the computer. \n
+
+      Well, still now I spend most of my time doing stuff in front of a computer, bashing command lines on
+      the terminal, playing on linux, studying new web technologies and frameworks.
+      I tend to to write and code for long hours, because I like the sound of my own fingers tapping keys.
+      But most of all I'm on a path to master JavaScript. I'm consistently learning new things everyday about programming.
+      `,
+
+      about_the_web : ` This is a place where I record my thoughts and discoveries which are more or less related to programming and stuff that I like.
+      This website was built with VueJs and runs on github pages. Right now, I'm still improving this site,
+      but I'm still busy from all the  projects I've been receiving lately. So thank you for dropping by and understanding.
+      `
     }
   }
 }
