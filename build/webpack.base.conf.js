483b0b19bd6f4454f0faf6825ed0132f1790bf7d
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-05-30 16:33:34 +0800

    Fix vue particle error in build

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,7 +36,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/vue-particles')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
