4fecb0f620f99e6d0400583d3f399501a4b6e9e1
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-10 23:21:15 +0800

    Update paragraph structure.
    Actual paragraphs to be replaced

--- src/pages/skills.vue
+++ src/pages/skills.vue
@@ -2,9 +2,13 @@
   <main class="article skill-page">
     <h1>My Skills</h1>
     <div class="container">
-      <p class="content">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+      <!-- <p class="content"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p> -->
+      <div class="content">
+        <p v-for="item in phrases">
+          {{ item }}
+        </p>
+      </div>
 
     <skill-manager />
     </div>
@@ -15,6 +19,16 @@
 
 <script>
 export default {
+  data(){
+    return {
+      phrases : [
+        `Here are the list of stuff that I've played, dabbled and cried during my journey in Programming.`,
+        'In the Front End, I use Javascript in building User Interface and client side logic',
+        'In the Back End, I use PHP in handling server side logic',
+        'And Python for creating script to automate testings of APIs and other more.'
+      ]
+    }
+  }
 }
 </script>
 
