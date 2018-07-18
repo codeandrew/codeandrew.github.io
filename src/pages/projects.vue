16b725696355b9e1cb609c3dbeda5c5967f9c937
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-18 17:36:26 +0800

    Update projects page. change gif

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -3,13 +3,23 @@
     <div class="container">
       <h2> Page still in construction</h2>
 
-      <img id="walker" src="http://media.giphy.com/media/XGnWMiVXL87Xa/giphy.gif">
+      <!-- <img id="walker" :src="blackWalking_light"> -->
+      <!-- <img id="walker" :src="blackWalking"> -->
+      <img id="walker" :src="blackWheel">
     </div>
   </main>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      blackWalking_light : "https://media.giphy.com/media/eLOQ97wvI6bzG/giphy.gif",
+      blackWalking : 'https://media.giphy.com/media/HecFZe3RGvHDq/giphy.gif',
+      blackWheel : 'https://media.giphy.com/media/Xz5fPy9BGr3r2/giphy.gif'
+    }
+  },
+
   mounted(){
     // Get the walker image:
     var walker = document.getElementById('walker');
@@ -21,7 +31,7 @@ export default {
 
     function check(){
       position += walkingLeft ? dist : -dist;
-      walker.style.right = position +'px';
+      walker.style.left = position +'px';
       if (position > leftBorder || position < rightBorder){
         walkingLeft = !walkingLeft;
         walker.classList.toggle('flip')
