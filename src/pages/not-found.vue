f5e3817d7e505861a130d5a889036d720b110603
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-19 18:59:33 +0800

    Use placeholder component in not found and projects

--- src/pages/not-found.vue
+++ src/pages/not-found.vue
@@ -2,9 +2,7 @@
   <main class="article not-found">
     <div class="container">
       <h1>Error 404</h1>
-      <h2> Page not found </h2>
-      <!-- <img id="walker" src="http://media.giphy.com/media/XGnWMiVXL87Xa/giphy.gif"> -->
-      <img id="walker" src="https://media.giphy.com/media/Xz5fPy9BGr3r2/giphy.gif">
+      <placeholder :subheader="subheader" />
       <h4>Let's get outta here </h4>
     </div>
   </main>
@@ -12,24 +10,10 @@
 
 <script>
 export default {
-  mounted(){
-    // Get the walker image:
-    const walker = document.getElementById('walker');
-    let walkingLeft = true;
-    let dist = 6;
-    let leftBorder = 600;
-    let rightBorder = 10;
-    let position = rightBorder;
-
-    function check(){
-      position += walkingLeft ? dist : -dist;
-      walker.style.left = position +'px';
-      if (position > leftBorder || position < rightBorder){
-        walkingLeft = !walkingLeft;
-        walker.classList.toggle('flip')
-      }
+  data(){
+    return {
+      subheader : 'Page not found'
     }
-    setInterval(check, 70)
   }
 }
 </script>
--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,8 +1,7 @@
 <template lang="html">
   <main class="article projects">
     <div class="container">
-      <h2> Page still in construction</h2>
-      <img id="walker" :src="blackWheel">
+      <placeholder :subheader="subheader" />
     </div>
   </main>
 </template>
@@ -11,31 +10,10 @@
 export default {
   data(){
     return {
-      blackWalking_light : "https://media.giphy.com/media/eLOQ97wvI6bzG/giphy.gif",
-      blackWalking : 'https://media.giphy.com/media/HecFZe3RGvHDq/giphy.gif',
-      blackWheel : 'https://media.giphy.com/media/Xz5fPy9BGr3r2/giphy.gif'
+      subheader : 'Page still in construction'
     }
   },
 
-  mounted(){
-    // Get the walker image:
-    var walker = document.getElementById('walker');
-    var walkingLeft = true;
-    var dist = 6;
-    var leftBorder = 600;
-    var rightBorder = 10;
-    var position = rightBorder;
-
-    function check(){
-      position += walkingLeft ? dist : -dist;
-      walker.style.left = position +'px';
-      if (position > leftBorder || position < rightBorder){
-        walkingLeft = !walkingLeft;
-        walker.classList.toggle('flip')
-      }
-    }
-    setInterval(check, 70)
-  }
 }
 </script>
 
