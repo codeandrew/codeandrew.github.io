ef152fc51e539db1c5202220ee0824c75de3d260
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-07-23 20:36:30 +0800

    Implement heading on h1 skills vue

--- src/pages/skills.vue
+++ src/pages/skills.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <main class="article skill-page">
-    <h1>My Skills</h1>
+    <h1 class="heading">My Skills</h1>
     <div class="container">
       <!-- <p class="content"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p> -->
